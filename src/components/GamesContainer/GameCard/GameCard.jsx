a865e8d58eacb04138804e317ef03b85f468aadd
valselviz <valeriaselviz@gmail.com>
2023-09-24 15:47:54 -0300

    feat: GamesContainer

--- src/components/GamesContainer/GameCard/GameCard.jsx
+++ src/components/GamesContainer/GameCard/GameCard.jsx
@@ -2,7 +2,7 @@ import './GameCard.css';
 import GameCardImage from './GameCardImage/GameCardImage';
 import GameCardInfo from './GameCardInfo/GameCardInfo';
 
-export default function GameCard() {
+export default function GameCard(props) {
 
     return (
         <div className="card">
---
+++ src/components/GamesContainer/GamesContainer.jsx
@@ -0,0 +1,12 @@
+import './GamesContainer.css';
+import GameCard from './GameCard/GameCard';
+
+export default function GamesContainer() {
+
+
+    return (
+        <div className="GamesContainer.css">
+            <GameCard />
+        </div>
+    ) 
+}
