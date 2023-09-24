6472152066e935eec2950fd71a368450c69ec609
valselviz <valeriaselviz@gmail.com>
2023-09-24 18:03:48 -0300

    feat: fetching data

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -2,24 +2,29 @@ import { useState, useEffect } from 'react';
 
 export default function fetchData(url) {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
 
-    useEffect(() => {
-        setLoading(true);
-        try {
-            fetch(url)
-                .then((res) => res.json())
-                .then((data) => setData(data));
-        } catch (e) {
-            throw new Error(
-                setError(
-                    `Cannot fetch the data, error with status ${response.status}`
-                )
-            );
-        } finally {
-            setLoading(false);
+    const rawgioApiKey = ""
+    const requestOptions = {
+        headers: {
+            "Target-URL": "https://rawg.io",
+            "Authorization": "" // the cors-proxy expects this attribute, otherwise it fails
         }
+    }
+
+    useEffect(() => {
+        fetch(`${url}?key=${rawgioApiKey}`, requestOptions)
+            .then((res) => res.json())
+            .then((data) => setData(data))
+            .catch(error => {
+                throw new Error(
+                    setError(
+                        `Cannot fetch the data, error with status ${error}`
+                    )
+                )
+            })
+            .finally(() => setLoading(false))
     }, [loading]);
 
     return { data, loading, error };
--- src/components/GamesContainer/GamesContainer.jsx
+++ src/components/GamesContainer/GamesContainer.jsx
@@ -1,12 +1,16 @@
 import './GamesContainer.css';
 import GameCard from './GameCard/GameCard';
+import fetchData from '../../api/fetchData';
 
 export default function GamesContainer() {
+    console.log('hola')
+    const result = fetchData('http://localhost:3001/api/games')
 
+    console.log(result)
 
     return (
         <div className="GamesContainer.css">
-            <GameCard />
+            {result.loading ? "" : result.data.results.map((game, index) => <GameCard key={index}/>)}
         </div>
     ) 
 }
