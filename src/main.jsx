842f94563ad06d319ccb5984b75a47de1e86f7a9
Francisco Gomensoro <102269369+fgomensoro@users.noreply.github.com>
2023-09-28 15:36:49 -0300

    feat: 49 save session with cookies and pages authorization (#10)
    * feat: save session with cookie
    
    * feat: authorization with cookie session
    
    * fix: cookie util
    
    * fix: useisloggedin and useisloggedout in one function
    
    * feat: login functionality in login component

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,31 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
-import LastSearches from './pages/LastSearches';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
-import Register from './pages/Register';
-import ForgotPassword from './pages/ForgotPassword';
+import Layout from '@/components/Layout/Layout';
+import UseIsLoggedIn from '@/hooks/UseIsLoggedIn';
+import ForgotPassword from '@/pages/ForgotPassword';
+import Home from '@/pages/Home';
+import LastSearches from '@/pages/LastSearches';
+import Login from '@/pages/Login';
+import PageNotFound from '@/pages/PageNotFound';
+import Register from '@/pages/Register';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="*" element={<PageNotFound />} />
+            <Route element={<UseIsLoggedIn needAccess={false} />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Route>
+            <Route element={<UseIsLoggedIn needAccess={true} />}>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/last-searches" element={<LastSearches />} />
+                </Route>
+            </Route>
             <Route path="/" element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/last-searches" element={<LastSearches />} />
+                <Route path="*" element={<PageNotFound />} />
             </Route>
         </>,
     ),
