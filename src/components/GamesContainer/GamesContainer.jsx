2f52f3aab41066318682c31870c5ddf78d5b683f
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-03 18:55:32 -0300

    fix: update fetch path and add data state

--- src/components/GamesContainer/GamesContainer.jsx
+++ src/components/GamesContainer/GamesContainer.jsx
@@ -1,14 +1,21 @@
-import fetchGames from '../../api/fetchGames';
+import fetchGames from '@api/fetchGames';
 import GameCard from './GameCard/GameCard';
+import { useEffect, useState } from 'react';
 
 export default function GamesContainer() {
-    const { loading, data } = fetchGames('https://rawg.io/api/games?token');
+    const [result, setResult] = useState({ loading: true, data: {}, error: null });
 
-    return (
-        <div>
-            {loading
-                ? ''
-                : data?.results?.map((game, index) => <GameCard game={game} key={index} ranking={index + 1} />)}
-        </div>
-    );
+    /* Fetches games data and set the result to state */
+    useEffect(() => {
+        const getGamesData = async () => await fetchGames('https://rawg.io/api/games/');
+        getGamesData()
+            .then((result) => setResult(result))
+            .catch((err) => setResult(err));
+    }, []);
+
+    const gameCards = result?.data?.results?.map((game, index) => (
+        <GameCard game={game} key={index} ranking={index + 1} />
+    ));
+
+    return <div>{!result.loading && gameCards}</div>;
 }
