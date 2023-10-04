08d02dd40d25ec493e37577d91ef46c5901a22b5
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-03 22:28:51 -0300

    Fix/fetch functionality (#19)
    * refactor: remove useEffect - use async/await func
    
    * fix: update fetch path and add data state

--- src/api/fetchGames.js
+++ src/api/fetchGames.js
@@ -1,32 +1,29 @@
-import { useEffect } from 'react';
-
-export default function fetchGames(url, searchQuery, page, gameId) {
-    const result = { loading: false, data: {}, error: null };
-
+export default async function fetchGames(url, searchQuery, page, gameId) {
     const apiKey = import.meta.env.VITE_API_KEY;
     const corsProxy = import.meta.env.VITE_CORS_PROXY;
+    const result = { loading: true, data: {}, error: null };
 
-    let route = `${corsProxy}${url}&key=${apiKey}`;
+    let route = `${corsProxy}${url}?key=${apiKey}`;
     route = searchQuery ? `${corsProxy}${url}?key=${apiKey}&search=${searchQuery}` : route;
     route = page ? `${route}&page=${page}` : route;
     route = gameId ? `${corsProxy}${url}${gameId}?key=${apiKey}` : route;
 
-    useEffect(() => {
-        fetch(route)
-            .then((res) => {
-                if (!res.ok) {
-                    result.error = `Request failed with status: ${res.status}`;
-                    throw new Error(result.error);
-                }
-                return res.json();
-            })
-            .then((data) => (result.data = data))
-            .catch((error) => {
-                result.error = `Cannot fetch the data, error with status ${error}`;
-                throw new Error(result.error);
-            })
-            .finally(() => (result.loading = false));
-    }, [result.loading]);
+    try {
+        const response = await fetch(route);
 
-    return result;
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        result.data = data;
+        result.loading = false;
+
+        return result;
+    } catch (error) {
+        result.error = error;
+        result.loading = false;
+
+        return result;
+    }
 }
