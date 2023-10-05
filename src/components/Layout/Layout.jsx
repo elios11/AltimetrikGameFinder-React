db0c0c40f24b235eeabbdc655bf427a8f7654b2d
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-04 22:34:33 -0300

    refactor: remove unnecessary title

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
     return (
         <div>
-            <h3>Layout</h3>
             <Outlet />
         </div>
     );
