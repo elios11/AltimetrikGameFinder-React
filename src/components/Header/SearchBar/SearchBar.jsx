9ada7c0193af3bf994a213af0e5a80b7b820f9dd
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-11 00:28:44 -0300

    style: add styles for desktop view

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -5,9 +5,14 @@ import clearSearchBtn from '@assets/icons/close-x-btn.svg';
 export default function SearchBar() {
     return (
         <div className={styles['search-bar']}>
-            <img src={searchBarIcon} alt="search bar icon" />
-            <input id="searchBar" type="text" placeholder="Search games..." />
-            <img id="clearSearchBtn" src={clearSearchBtn} alt="clear search button" />
+            <img className={styles['search-bar__icon']} src={searchBarIcon} alt="search bar icon" />
+            <input className={styles['search-bar__input']} id="searchBar" type="text" placeholder="Search games..." />
+            <img
+                className={styles['search-bar__clear-search-btn']}
+                id="clearSearchBtn"
+                src={clearSearchBtn}
+                alt="clear search button"
+            />
         </div>
     );
 }
