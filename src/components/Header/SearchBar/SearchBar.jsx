3c7943dc045657a2d74057029f14aa7d02101929
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-12 13:30:24 -0300

    70 header view desktop (#32)
    * feat: add log out button
    
    * style: add styles for desktop view

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -5,6 +5,14 @@ import clearSearchBtn from '@assets/icons/close-x-btn.svg';
 export default function SearchBar() {
     return (
         <div className={styles['search-bar']}>
+            <img className={styles['search-bar__icon']} src={searchBarIcon} alt="search bar icon" />
+            <input className={styles['search-bar__input']} id="searchBar" type="text" placeholder="Search games..." />
+            <img
+                className={styles['search-bar__clear-search-btn']}
+                id="clearSearchBtn"
+                src={clearSearchBtn}
+                alt="clear search button"
+            />
             <img src={searchBarIcon} alt="search bar icon" />
             <input id="searchBar" type="text" placeholder="Search games..." />
             <img id="clearSearchBtn" src={clearSearchBtn} alt="clear search button" />
