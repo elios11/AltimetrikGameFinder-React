37e8096193ea63403fc6efc21d347eda62c84ebe
valselviz <134339028+valselviz@users.noreply.github.com>
2023-10-18 15:30:40 -0300

    8 switch card columns (#35)
    * chore: adding CSS for single column style
    
    * chore: implementing context

---
+++ src/context/SingleColumnContext.jsx
@@ -0,0 +1,20 @@
+import { useState, createContext } from 'react';
+import PropTypes from 'prop-types';
+
+const SingleColumnContext = createContext(null);
+
+export function SingleColumnContextProvider({ children }) {
+    const [singleColumn, setSingleColumn] = useState(false);
+
+    return (
+        <SingleColumnContext.Provider value={{ singleColumn, setSingleColumn }}>
+            {children}
+        </SingleColumnContext.Provider>
+    );
+}
+
+SingleColumnContextProvider.propTypes = {
+    children: PropTypes.element.isRequired,
+};
+
+export default SingleColumnContext;
