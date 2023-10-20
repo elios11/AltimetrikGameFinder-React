e21cf2854c58920fb024b03163ac807b3a11f56e
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-20 19:13:34 -0300

    style: update names to use modal instead of dialog

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,39 +1,35 @@
 import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Dialog({ children, setIsDialogOpen, isDialogOpen, type }) {
-    const dialogRef = useRef(true);
+export default function Dialog({ children, closeModal, isModalOpen, type }) {
+    const modalRef = useRef(true);
 
     function handleDialogType() {
         if (type === 'modal') {
-            dialogRef.current?.showModal();
+            modalRef.current?.showModal();
         } else if (type === 'dialog') {
-            dialogRef.current?.open();
+            modalRef.current?.open();
         }
     }
 
     useEffect(() => {
-        if (isDialogOpen) {
+        if (isModalOpen) {
             handleDialogType();
         } else {
-            dialogRef.current?.close();
+            modalRef.current?.close();
         }
-    }, [isDialogOpen, type]);
-
-    function closeDialog() {
-        setIsDialogOpen(false);
-    }
+    }, [isModalOpen, type]);
 
     return (
-        <dialog ref={dialogRef} onClose={closeDialog}>
+        <dialog ref={modalRef} onClose={closeModal}>
             {children}
         </dialog>
     );
 }
 
 Dialog.propTypes = {
-    children: PropTypes.node,
-    setIsDialogOpen: PropTypes.func,
-    isDialogOpen: PropTypes.bool,
-    type: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    isModalOpen: PropTypes.bool.isRequired,
+    type: PropTypes.string.isRequired,
 };
