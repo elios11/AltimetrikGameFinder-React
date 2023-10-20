753a31e71159834ea498a915ccb70287717553d0
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-20 19:22:51 -0300

    fix: use conditional chain to show name

--- src/components/GameDataModal/GameDataModalInfo/GameDataModalInfo.jsx
+++ src/components/GameDataModal/GameDataModalInfo/GameDataModalInfo.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export default function GameDataModalInfo({ gameData }) {
-    return <>{gameData.name}</>;
+    return <>{gameData?.name}</>;
 }
 
 GameDataModalInfo.propTypes = {
