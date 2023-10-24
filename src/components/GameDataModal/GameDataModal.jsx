c1204e16a0907eb99950c07a5d5ecdc780fb1d47
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-23 22:23:10 -0300

    feat: add retrieval of game assets
    - Add styling for modal component
    
    - Add styles for game data modal component
    
    - Add modal onClose functionality

--- src/components/GameDataModal/GameDataModal.jsx
+++ src/components/GameDataModal/GameDataModal.jsx
@@ -1,40 +1,71 @@
 import { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import styles from './GameDataModal.module.css';
 
 import fetchGames from '@api/fetchGames';
 import Dialog from '@components/Dialog';
 import GameDataModalInfo from './GameDataModalInfo/GameDataModalInfo';
 import RequestsContext from '@context/RequestsContext';
+import fetchGameAssets from '@api/fetchGameAssets';
 
-export default function GameDataModal({ isModalOpen, closeModal, modalGameId }) {
-    const [gameData, setGameData] = useState();
-    const { gamesDescription } = useContext(RequestsContext);
+export default function GameDataModal({ isModalOpen, closeModal, openModal, setModalGameId, modalGameId }) {
+    const [gameData, setGameData] = useState(null);
+    const { gamesDescription, setGameAssets, gameAssets } = useContext(RequestsContext);
+    const dialogElement = document.querySelector(`.${styles.dialog}`);
+
+    const modalBackground = `
+        linear-gradient(
+            180deg,
+            rgba(48, 48, 48, 0),
+            rgba(48, 48, 48, 1) var(--background-img-height)
+        ),
+        url("${gameData?.background_image}"`;
+
+    if (dialogElement) {
+        dialogElement.style.setProperty('background-image', modalBackground);
+    }
 
     useEffect(() => {
         if (!modalGameId) {
             return;
         }
 
+        fetchGameAssets(modalGameId)
+            .then((result) => setGameAssets(result))
+            .catch((e) => setGameAssets(e));
+
         if (gamesDescription[modalGameId]) {
             setGameData(gamesDescription[modalGameId]);
+            openModal();
         } else {
             fetchGames('https://rawg.io/api/games/', null, null, modalGameId)
                 .then((result) => {
                     setGameData(result.data);
+                    openModal();
                 })
                 .catch((err) => setGameData(err));
         }
     }, [modalGameId]);
 
+    function onDialogClose() {
+        closeModal();
+        setModalGameId(null);
+        setGameData(null);
+    }
+
     return (
-        <Dialog type="modal" isModalOpen={isModalOpen} closeModal={closeModal}>
-            <GameDataModalInfo gameData={gameData} />
-        </Dialog>
+        modalGameId && (
+            <Dialog type="modal" isModalOpen={isModalOpen} onClose={onDialogClose} customStyles={styles.dialog}>
+                <GameDataModalInfo gameData={gameData} gameAssets={gameAssets} />
+            </Dialog>
+        )
     );
 }
 
 GameDataModal.propTypes = {
     isModalOpen: PropTypes.bool.isRequired,
     closeModal: PropTypes.func.isRequired,
-    modalGameId: PropTypes.string.isRequired,
+    openModal: PropTypes.func.isRequired,
+    setModalGameId: PropTypes.func.isRequired,
+    modalGameId: PropTypes.number,
 };
