229c3a9c3144bebec00200148bd4be49dc64aa53
ariadnaflores <ariadnafloresvilchez@gmail.com>
2023-10-24 18:14:10 -0500

    fix: remove sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,34 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar/Sidebar'; // Use lowercase 'sidebar'
+import { Outlet, useLocation } from 'react-router-dom';
+import { RequestsContextProvider } from '@context/RequestsContext';
+import { SingleColumnContextProvider } from '@context/SingleColumnContext';
+import MainSection from '@components/MainSection/MainSection';
+import Header from '../Header/Header';
+import styles from './Layout.module.css';
 
 export default function Layout() {
+    const location = useLocation();
+    const latestSearches = location.pathname === '/last-searches';
+    const title = latestSearches ? 'Latest searches' : 'New and trending';
+    const subtitle = latestSearches ? '' : 'Based on player counts and release date';
+
     return (
         <div>
-            <Sidebar/>
-            <Outlet />
+            <RequestsContextProvider>
+                <SingleColumnContextProvider>
+                    <div className={styles.layoutContainer}>
+                        <div className={styles.header}>
+                            <Header />
+                        </div>
+                        <div className={styles.body}>
+                            <div className={styles.main}>
+                                <MainSection title={title} subtitle={subtitle} />
+                            </div>
+                            <Outlet />
+                        </div>
+                        <div className={styles['fading-bottom']}></div>
+                    </div>
+                </SingleColumnContextProvider>
+            </RequestsContextProvider>
         </div>
     );
 }
