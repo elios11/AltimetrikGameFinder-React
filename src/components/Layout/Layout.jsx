b65e5412c7786c91cbc8a7908b3f5a66e10afd92
Elías Ferreira <eliosferre3@hotmail.com>
2023-11-09 22:43:46 -0300

    refactor: move sidebar and gameModal to layout
    - add outlet context
    
    - handle sidebar logic from layout
    
    - add state for choosing title, subtitle and column buttons

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,27 @@
-import { Outlet, useLocation } from 'react-router-dom';
-
-import MainSection from '@components/MainSection/MainSection';
+import styles from './Layout.module.css';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import { RequestsContextProvider } from '@context/RequestsContext';
 import { SingleColumnContextProvider } from '@context/SingleColumnContext';
 
+import MainSection from '@components/MainSection/MainSection';
+import SidebarNav from '@components/SidebarNav/SidebarNav';
 import Header from '../Header/Header';
-import styles from './Layout.module.css';
+import GameDataModal from '@components/GameDataModal/GameDataModal';
 
 export default function Layout() {
-    const location = useLocation();
-    const latestSearches = location.pathname === '/last_searches';
-    const title = latestSearches ? 'Latest searches' : 'New and trending';
-    const subtitle = latestSearches ? '' : 'Based on player counts and release date';
-    const columnButtons = !latestSearches;
+    const [title, setTitle] = useState();
+    const [subtitle, setSubtitle] = useState();
+    const [columnButtons, setColumnButtons] = useState();
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const closeSidebar = () => setIsSidebarOpen(false);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalGameId, setModalGameId] = useState(null);
+
+    const closeModal = () => setIsModalOpen(false);
+    const openModal = () => setIsModalOpen(true);
 
     return (
         <div>
@@ -20,15 +29,29 @@ export default function Layout() {
                 <SingleColumnContextProvider>
                     <div className={styles.layoutContainer}>
                         <div className={styles.header}>
-                            <Header />
+                            <Header isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
                         </div>
                         <div className={styles.body}>
                             <div className={styles.main}>
                                 <MainSection title={title} subtitle={subtitle} columnButtons={columnButtons} />
                             </div>
-                            <Outlet />
+                            <div className={styles['main__body-content']}>
+                                <div className={styles['body__sidebar']}>
+                                    <SidebarNav setIsSidebarOpen={setIsSidebarOpen} />
+                                </div>
+                                <Outlet
+                                    context={{ closeSidebar, setModalGameId, setTitle, setSubtitle, setColumnButtons }}
+                                />
+                            </div>
                         </div>
                         <div className={styles['fading-bottom']}></div>
+                        <GameDataModal
+                            isModalOpen={isModalOpen}
+                            openModal={openModal}
+                            closeModal={closeModal}
+                            setModalGameId={setModalGameId}
+                            modalGameId={modalGameId}
+                        />
                     </div>
                 </SingleColumnContextProvider>
             </RequestsContextProvider>
