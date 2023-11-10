e40de9d47d18d06e78bdc3199ad76a21c0b3a403
Elías Ferreira <eliosferre3@hotmail.com>
2023-11-09 22:50:28 -0300

    refactor: move sidebar to layout

--- src/pages/LastSearches.jsx
+++ src/pages/LastSearches.jsx
@@ -1,19 +1,10 @@
 import LastSearches from '@components/LastSearches/LastSearches';
-import SidebarNav from '@components/SidebarNav/SidebarNav';
 import styles from './LastSearchesPage.module.css';
-import homeStyles from './Home.module.css';
 
 export default function LastSearchesPage() {
     return (
-        <>
-            <div className={styles['last-searches-container']}>
-                <div className={`${homeStyles['home__sidebar']} ${styles['last-searches-container__sidebar']}`}>
-                    <SidebarNav />
-                </div>
-                <div className={styles['last-searches']}>
-                    <LastSearches />
-                </div>
-            </div>
-        </>
+        <div className={styles['last-searches']}>
+            <LastSearches />
+        </div>
     );
 }
